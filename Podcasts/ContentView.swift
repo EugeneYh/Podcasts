e548d152a53dac86160ce2714b3ba53c95c3dda5
Eugene Y <yehanovskiy@gmail.com>
2020-05-03 17:53:57 +0300

    Initial Commit

---
+++ Podcasts/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Podcasts
+//
+//  Created by Eugene on 03.05.2020.
+//  Copyright © 2020 Eugene. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
